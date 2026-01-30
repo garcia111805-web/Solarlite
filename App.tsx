b6d3ade7a5da615551a3eb60c6d99737371ecd0f
garcia111805-web <garcia111805@gmail.com>
2026-01-31 01:11:26 +0800

    Refactor routing from HashRouter to BrowserRouter

--- App.tsx
+++ App.tsx
@@ -1,29 +1,25 @@
 
-import React from 'react';
-import { HashRouter, Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
-import Home from './pages/Home';
-import Services from './pages/Services';
-import Projects from './pages/Projects';
-import About from './pages/About';
-import Contact from './pages/Contact';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// These paths assume your files are named Index.tsx and Qualified.tsx 
+// inside a folder named 'pages'.
+import Index from './pages/Index'; 
 import Qualified from './pages/Qualified';
 
-const App: React.FC = () => {
+function App() {
   return (
-    <HashRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/qualified" element={<Qualified />} />
-        </Routes>
-      </Layout>
-    </HashRouter>
+    <Router>
+      <Routes>
+        {/* If the URL is just your site name, show the Home page */}
+        <Route path="/" element={<Index />} />
+
+        {/* If the URL has /qualified at the end, show the Qualified page */}
+        <Route path="/qualified" element={<Qualified />} />
+        
+        {/* This is a fallback: if someone types a random URL, send them Home */}
+        <Route path="*" element={<Index />} />
+      </Routes>
+    </Router>
   );
-};
+}
 
 export default App;
